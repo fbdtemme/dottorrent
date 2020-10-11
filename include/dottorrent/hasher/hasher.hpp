59a3fb888eb82e5356f38487cd51931597e46287
fbdtemme <floriandetemmerman@gmail.com>
2020-10-11 12:43:24 +0200

    add update from std::string_view

--- include/dottorrent/hasher/hasher.hpp
+++ include/dottorrent/hasher/hasher.hpp
@@ -2,8 +2,6 @@
 #include <span>
 #include <cstddef>
 
-//#include "dottorrent/hash_function.hpp"
-
 
 namespace dottorrent {
 
@@ -12,9 +10,14 @@ class hasher
 public:
     virtual void update(std::span<const std::byte> data) = 0;
 
+    virtual void update(std::string_view data)
+    {
+        auto s = std::span{reinterpret_cast<const std::byte*>(data.data()), data.size()};
+        update(s);
+    }
+
     virtual void finalize_to(std::span<std::byte> out) = 0;
 
     virtual ~hasher() = default;
 };
-
 }
