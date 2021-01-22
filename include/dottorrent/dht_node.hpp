decf3b57e14b91d1df33718153bc55b902e47f65
fbdtemme <floriandetemmerman@gmail.com>
2021-01-22 15:35:56 +0100

    Add structured binding support to dht_node

--- include/dottorrent/dht_node.hpp
+++ include/dottorrent/dht_node.hpp
@@ -2,6 +2,8 @@
 #include <compare>
 #include <cstdint>
 #include <string>
+#include <tuple>
+#include <type_traits>
 
 namespace dottorrent {
 
@@ -23,6 +25,39 @@ struct dht_node
 
     bool operator==(const dht_node& that) const = default;
     std::strong_ordering operator<=>(const dht_node& that) const  = default;
+
+    // friends for structured binding support
+    template <std::size_t N> friend const auto& get(const dht_node&);
+    template <std::size_t N> friend auto& get(dht_node&);
 };
 
 } // namespace dottorrent
+
+//-----------------------------------------------------------------------------//
+// Structured bindings support                                                 //
+//-----------------------------------------------------------------------------//
+
+namespace dottorrent {
+template <std::size_t N>
+const auto& get(const dht_node& a) {
+    if      constexpr (N == 0) return a.url;
+    else if constexpr (N == 1) return a.port;
+}
+
+template <std::size_t N>
+auto& get(dht_node& a) {
+    if      constexpr (N == 0) return a.url;
+    else if constexpr (N == 1) return a.port;
+}
+}
+
+namespace std {
+template<>
+struct tuple_size<dottorrent::dht_node>
+        : std::integral_constant<std::size_t, 2> {};
+
+template<std::size_t N>
+struct tuple_element<N, dottorrent::dht_node> {
+    using type = decltype(get<N>(std::declval<dottorrent::dht_node>()));
+};
+}
