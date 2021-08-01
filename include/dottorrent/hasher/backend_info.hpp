9ccdfb0d153a980918c34722e5c85f57513ed328
fbdtemme <floriandetemmerman@gmail.com>
2021-08-01 10:54:52 +0200

    Add function returning versions of hashing backends

---
+++ include/dottorrent/hasher/backend_info.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#if defined(DOTTORRENT_USE_OPENSSL)
+#include <openssl/crypto.h>
+#endif
+
+#if defined(DOTTORRENT_USE_ISAL)
+#include <isa-l_crypto.h>
+#endif
+
+#if defined(DOTTORRENT_USE_GCRYPT)
+#include <gcrypt.h>
+#endif
+
+#if defined(DOTTORRENT_USE_WOLFSSL)
+#include <wolfssl/version.h>
+#endif
+
+#if defined(DOTTORRENT_USE_WINCNG)
+#include <bcrypt.h>
+#endif
+
+#define MAKE_VERSION_STRING(major, minor, patch) STR(major) "." STR(minor) "." STR(patch)
+
+namespace dottorrent {
+
+inline std::map<std::string, std::string>
+cryptographic_backends()
+{
+    std::map<std::string, std::string> versions;
+
+#if defined(DOTTORRENT_USE_OPENSSL)
+    versions["openssl"] = OPENSSL_VERSION_TEXT;
+#endif
+#if defined(DOTTORRENT_USE_ISAL)
+    versions["isa-l_crypto"] = MAKE_VERSION_STRING(ISAL_CRYPTO_MAJOR_VERSION, ISAL_CRYPTO_MINOR_VERSION, ISAL_CRYPTO_PATCH_VERSION);
+#endif
+#if defined(DOTTORRENT_USE_GCRYPT)
+    versions["gcrypt"] = GCRYPT_VERSION;
+#endif
+#if defined(DOTTORRENT_USE_WOLFSSL)
+    versions["wolfssl"] = LIBWOLFSSL_VERSION_STRING;
+#endif
+#if defined(DOTTORRENT_USE_WINCNG)
+    versions["wincng"] = BCRYPT_HASH_INTERFACE_VERSION_1;
+#endif
+
+    return versions;
+}
+
+} // namespace dottorrent
---
+++ tests/hashers/test_cryptographic_backends.cpp
@@ -0,0 +1,30 @@
+//
+// Created by fbdtemme on 8/1/21.
+//
+
+#include <catch2/catch.hpp>
+#include <dottorrent/hasher/backend_info.hpp>
+
+namespace dt = dottorrent;
+
+
+TEST_CASE("test crypto backends versions")
+{
+    auto versions = dt::cryptographic_backends();
+
+#if defined(DOTTORRENT_USE_OPENSSL)
+    CHECK(versions.contains("openssl"));
+#endif
+#if defined(DOTTORRENT_USE_ISAL)
+    CHECK(versions.contains("isa-l_crypto"));
+#endif
+#if defined(DOTTORRENT_USE_GCRYPT)
+    CHECK(versions.contains("gcrypt"));
+#endif
+#if defined(DOTTORRENT_USE_WOLFSSL)
+    CHECK(versions.contains("wolfssl"));
+#endif
+#if defined(DOTTORRENT_USE_WINCNG)
+    CHECK(versions.contains("wincng"));
+#endif
+}
